18a2ffb146f582c58190496fd931cdd7ab4fe595
jbrooks036 <joannbrooks036@gmail.com>
2015-05-04 20:47:55 -0500

    simple FizzBuzz in C#

---
+++ FizzBuzz-CS/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz_CS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i < 101; i++)
+            {
+                Console.Write(i + " ");
+                if (i % 3 == 0)
+                {
+                    Console.Write("Fizz");
+                }
+                if (i % 5 == 0)
+                {
+                    Console.Write("Buzz");
+                }
+                Console.WriteLine("");
+            }
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey(); 
+        }
+    }
+}
